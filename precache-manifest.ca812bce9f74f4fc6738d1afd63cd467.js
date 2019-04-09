4309347cc0de1610587fd04142d61496d5aa0d99
jdguyton <jd.guyton@gmail.com>
2019-04-08 22:40:58 -0500

    Updates

---
+++ precache-manifest.ca812bce9f74f4fc6738d1afd63cd467.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "123db8eb6abcfcabd4d6",
+    "url": "/clicky-game/static/js/main.e7fc09ca.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "123db8eb6abcfcabd4d6",
+    "url": "/clicky-game/static/css/main.a0cf1e75.chunk.css"
+  },
+  {
+    "revision": "26e51aaeb55b436e36412da7eeed9d1e",
+    "url": "/clicky-game/index.html"
+  }
+];
